01345b117573b79d59b7b8b0da3a65fffd9ba198
asad <mdasadul@ualberta.ca>
2018-04-01 09:05:03 -0400

    adding encoder and decoder

--- model.py
+++ model.py
@@ -17,46 +17,47 @@ def BaseModel(object):
 		self.target_sequence_length = hparams.target_sequence_length
 		self.num_layers = hparams.num_layers
 
+
+		return_loss = _build_graph(rnn_size, num_layers, dropout,seed)
+
+
+	def _build_graph(self, rnn_size, num_layers, dropout,seed):
+		encoder_cell()
 		
-		return_loss = _build_graph()
+
+	def encoder_cell(self, rnn_size, dropout,num_layers)
+		# foward 
+		cell_fw = tf.nn.rnn_cell.MultiRNNCell([_make_lstm(rnn_size, keep_prob) for _ in range(n_layers)])
+		# backward    
+		cell_bw = tf.nn.rnn_cell.MultiRNNCell([make_lstm(rnn_size, keep_prob) for _ in range(n_layers)])
+		enc_output, enc_state = tf.nn.bidirectional_dynamic_rnn(cell_fw,
+															cell_bw,
+															rnn_inputs,
+															sequence_length=sequence_length,
+															dtype=tf.float32,
+															)
+		enc_output = tf.concat(enc_output,-1)
+
+	# helper to create the attention cell with
+	def decoder_cell(dec_cell, rnn_size, enc_output,  lengths):
+		attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(
+				num_units              = rnn_size,
+				memory                 = enc_output,
+				memory_sequence_length = lengths,
+				normalize                  = True,
+				name  = 'BahdanauAttention')
+
+		return  tf.contrib.seq2seq.AttentionWrapper(
+				cell                 = dec_cell,
+				attention_mechanism  = attention_mechanism,
+				attention_layer_size = rnn_size)
 
 
-	def _build_graph()
 	# helper to create the layers
-def make_lstm(rnn_size, keep_prob):
-lstm = tf.nn.rnn_cell.LSTMCell(rnn_size, initializer = tf.random_uniform_initializer(-0.1, 0.1, seed=2))
-lstm_dropout = tf.nn.rnn_cell.DropoutWrapper(lstm, input_keep_prob = keep_prob)
-return lstm_dropout
+	def _make_lstm(rnn_size, keep_prob,seed):
+		lstm = tf.nn.rnn_cell.LSTMCell(rnn_size, initializer = tf.random_uniform_initializer(-0.1, 0.1, seed=seed))
+		lstm_dropout = tf.nn.rnn_cell.DropoutWrapper(lstm, input_keep_prob = keep_prob)
+		return lstm_dropout
 
-# helper to create the attention cell with
-def decoder_cell(dec_cell, rnn_size, enc_output,  lengths):
-    attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(
-            num_units              = rnn_size,
-            memory                 = enc_output,
-            memory_sequence_length = lengths,
-            normalize                  = True,
-            name  = 'BahdanauAttention')
-
-    return  tf.contrib.seq2seq.AttentionWrapper(
-            cell                 = dec_cell,
-            attention_mechanism  = attention_mechanism,
-            attention_layer_size = rnn_size)
-
-# foward 
-cell_fw = tf.nn.rnn_cell.MultiRNNCell([make_lstm(rnn_size, keep_prob) for _ in range(n_layers)])
-
-
-
-# backward    
-cell_bw = tf.nn.rnn_cell.MultiRNNCell([make_lstm(rnn_size, keep_prob) for _ in range(n_layers)])
-
-
-
-enc_output, enc_state = tf.nn.bidirectional_dynamic_rnn(cell_fw,
-                                                        cell_bw,
-                                                        rnn_inputs,
-                                                        sequence_length=sequence_length,
-                                                        dtype=tf.float32,
-                                                        )
-
-enc_output = tf.concat(enc_output,-1)
+	
+	
